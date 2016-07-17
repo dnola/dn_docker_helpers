47f6d923fbf9862ee677a51021029e881b75fcfc
davidnola <dnola@scu.edu>
2016-07-16 23:59:07 -0700

    finalizing command

--- run_cifar10_x4.sh
+++ run_cifar10_x4.sh
@@ -5,3 +5,5 @@ docker pull dnola/hyperband_cifar
 sudo nvidia-docker volume setup
 NV_GPU=0 nvidia-docker run -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_0.txt & echo -e '' &
 NV_GPU=1 nvidia-docker run -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_1.txt & echo -e '' &
+NV_GPU=2 nvidia-docker run -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_2.txt & echo -e '' &
+NV_GPU=3 nvidia-docker run -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_3.txt & echo -e '' &
