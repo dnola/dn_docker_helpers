d846718413bb5573b1eafc28efea9174ad59cf63
davidnola <d.nola@me.com>
2016-07-18 17:15:23 -0700

    make sure docker is up to date

--- run_cifar10_x4.sh
+++ run_cifar10_x4.sh
@@ -3,6 +3,7 @@
 echo Starting Docker...
 docker pull dnola/hyperband_cifar
 sudo nvidia-docker volume setup
+sudo apt -y upgrade docker
 NV_GPU=0 nvidia-docker run --log-driver none -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_0.txt & echo -e '' &
 NV_GPU=1 nvidia-docker run --log-driver none -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_1.txt & echo -e '' &
 NV_GPU=2 nvidia-docker run --log-driver none -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_2.txt & echo -e '' &
