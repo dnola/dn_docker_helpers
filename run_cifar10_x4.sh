ef5f2ddd5e1e573545759985e7c0a09c5cb6db6e
davidnola <dnola@scu.edu>
2016-09-21 21:24:30 -0700

    finalizing command

--- run_cifar10_x4.sh
+++ run_cifar10_x4.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo Starting Docker...
+echo Starting Docker... > dockerstart.txt
 docker pull dnola/hyperband_cifar
+echo Finished Pulling Docker... > dockerpull.txt
 sudo nvidia-docker volume setup
-NV_GPU=0 nvidia-docker run -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_0.txt & echo -e '' &
-NV_GPU=1 nvidia-docker run -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_1.txt & echo -e '' &
-NV_GPU=2 nvidia-docker run -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_2.txt & echo -e '' &
-NV_GPU=3 nvidia-docker run -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_3.txt & echo -e '' &
+
+NV_GPU=0 nvidia-docker run --log-driver none -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_0.txt & echo -e '' &
+NV_GPU=1 nvidia-docker run --log-driver none -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_1.txt & echo -e '' &
+NV_GPU=2 nvidia-docker run --log-driver none -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_2.txt & echo -e '' &
+NV_GPU=3 nvidia-docker run --log-driver none -t -v $(pwd)/results/:/hyperband/results dnola/hyperband_cifar &> output_3.txt & echo -e '' &
